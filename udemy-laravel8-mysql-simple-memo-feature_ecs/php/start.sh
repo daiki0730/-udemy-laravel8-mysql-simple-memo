57061f4ed7777248d39e12e004a4815a501712c2
Daiki Nishida <46291510+daiki0730@users.noreply.github.com>
2022-12-22 20:49:07 +0900

    Delete udemy-laravel8-mysql-simple-memo-feature_ecs directory

--- udemy-laravel8-mysql-simple-memo-feature_ecs/php/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-php artisan migrate --force
-chmod -R 777 bootstrap 
-chmod -R 777 storage
-/usr/sbin/apache2ctl -D FOREGROUND
--- udemy-laravel8-mysql-simple-memo-feature_ecs/resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="card">
-    <div class="card-header">テストページ</div>
-</div>
-@endsection
